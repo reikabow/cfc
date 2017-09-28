e7cc1b8df1c743d88e4bf6ca076ffa7b8a6c9a81
Victor Sui <victorsui11@gmail.com>
2017-09-28 10:05:06 -0500

    support cors on backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const search = require('./search');
 
 const app = express();
 
+app.use(cors());
 app.use('/api/search', search);
 
 app.listen(process.env.PORT, () => {
