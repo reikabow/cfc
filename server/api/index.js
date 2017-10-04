0eb746ea310e64cfc7246488ee662a5997a728e5
Victor Sui <victorsui11@gmail.com>
2017-10-03 22:51:05 -0500

    add handlers for card routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
-const { SearchController } = require('./v1/controllers/');
+const SearchController = require('./v1/controllers/SearchController');
+const CardController = require('./v1/controllers/CardController');
 
 router.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 router.use('/search', SearchController.router);
+router.use('/cards', CardController.router);
 
 module.exports = router;
